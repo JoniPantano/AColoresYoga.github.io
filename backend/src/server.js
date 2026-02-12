b4988bf319d696d9ce78360698d862d28ebed50f
unknown <jonathanpantano17@gmail.com>
2026-02-12 07:45:04 -0300

    backend presentado

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({ message: "API funcionando correctamente" });
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
